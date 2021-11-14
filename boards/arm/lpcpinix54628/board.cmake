dd7ba8c1d6b5b835476a17bad2e954eca4116ec8
gasmaster <gasmaster2@naver.com>
2021-11-14 18:56:15 +0900

    - boards.cmake / dts 파일 수정

--- boards/arm/lpcpinix54628/board.cmake
+++ boards/arm/lpcpinix54628/board.cmake
@@ -1,5 +1,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
-#
+# SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(jlink "--device=LPC54628J512" "--speed=4000")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
