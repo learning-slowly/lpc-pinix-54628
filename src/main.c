003dd263ea3fedab7698f32f3164d3522ef008ec
gasmaster <gasmaster2@naver.com>
2021-11-19 00:44:27 +0900

    led blink 동작 구현 #1

--- src/main.c
+++ src/main.c
@@ -9,9 +9,27 @@
 #include <devicetree.h>
 #include <drivers/gpio.h>
 
+
 void main(void)
 {
+	const struct device *dev;
+	int ret;
+	dev = device_get_binding(DT_LABEL(DT_NODELABEL(gpio3)));
+	//dev = device_get_binding(DT_GPIO_LABEL(GPIO_3,gpios));
+	if(dev == NULL) {
+		return;
+	}
+		
+
+	ret = gpio_pin_configure(dev, 3, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
 	while (1) {
+		gpio_pin_set(dev, 3, 1);
+		k_msleep(1000);
+		gpio_pin_set(dev, 3, 0);
 		k_msleep(1000);
 	}
 }
